581faf5bbfd78fc838e139f482544f96e1e660dd
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-17 14:25:29 +0200

    add 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Description: va print une phrase
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
