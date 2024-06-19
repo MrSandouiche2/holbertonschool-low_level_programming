38f25034fb73f615c1ccfb4368c842a8f1330d5b
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-19 12:13:37 +0200

    made 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:print l'alphabet 
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i;
+char j;
+
+for (i = 'a'; i <= 'z'; i++)
+
+putchar(i);
+
+for (j = 'A'; j <= 'Z'; j++)
+
+putchar(j);
+
+return(0);
+}
