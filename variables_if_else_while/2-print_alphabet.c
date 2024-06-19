000449eba8300a3b50a41fda0fb04fad8903a2b7
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-19 10:52:42 +0200

    fixed betty

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: va print l'alphabet
+ *
+ *  * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char i;
 {
-for ( i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 	putchar(i);
 }
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
