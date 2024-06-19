9824091b1f2ac5629ea44009bb6e8502e94ec137
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-19 16:31:12 +0200

    made 6 and betty

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: va print les nombres de 0 à 9
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+
+{
+putchar('\n');
+
+return (0);
+}
+}
