25b1da5a2f5e4209a65fc4bffc9478d9dce0f5db
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-19 15:53:23 +0200

    made 5 and betty

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: va print les nombres de 0 à 9
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+
+return (0);
+}
