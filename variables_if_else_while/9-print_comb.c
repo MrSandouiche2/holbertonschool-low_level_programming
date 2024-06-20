7b418696183e5cd89bcfa8a2c4d32cd173476cda
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-20 14:40:53 +0200

    made 9 and betty

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: va print avec des virgules et espace
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if
+(i != '9');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+
+
+	return (0);
+}
