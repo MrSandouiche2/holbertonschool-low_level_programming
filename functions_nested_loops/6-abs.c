998f0d73999c5a2b16965147fabfa71160720702
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-21 15:47:31 +0200

    made 6 and betty

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - permet de display des abdos ??? (en fait c pour montrer l'absolu )
+ *@n: vieux truc x3
+ * Description: exo qui a duré 100 ans à faire
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+	return (n);
+}
+else if (n < 0)
+{
+	return (n * -1);
+}
+else
+return (0);
+}
