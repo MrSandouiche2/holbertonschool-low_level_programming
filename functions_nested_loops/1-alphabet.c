ee7b746dea801ae0b5fa7938dedd1efabc5a107d
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-21 11:03:14 +0200

    made 1 and betty

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - fonction
+ *
+ * Description: 'permet de print l'alphabet avec putchar
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int e;
+{
+for (e = 97; e <= 122; e++)
+
+	_putchar(e);
+
+return;
+}
+}
