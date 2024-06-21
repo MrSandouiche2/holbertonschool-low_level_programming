1c3f88c6ac89ba36c5330635009d542d4f3d924e
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-21 12:32:12 +0200

    made 3 and betty

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - fonction
+ *
+ *@c: vieux truc
+ * Description: 'permet de faire jsp quoi l'exercie m'enerve
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
