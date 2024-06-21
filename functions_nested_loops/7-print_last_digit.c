09809136ad880bae5d298f8483550a95fc88f75f
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-21 16:22:16 +0200

    added 7 and betty

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,22 @@
 #include "main.h"
-
-
+/**
+ * print_last_digit - bon je vais rien dire
+ *@n: vieux truc x4
+ * Description: exo qui a duré 1000 ans à faire
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 int print_last_digit(int n)
 {
 
-int i;
-	i = n % 10;
+int last;
 
-	if (i < 0)
-	{
-		i = -i;
-
-		_putchar('0' + i);
-	}
-	return (i);
+last = n % 10;
+if (last < 0)
+{
+	last = last * -1;
+}
+_putchar(last + '0');
+return (last);
 }
