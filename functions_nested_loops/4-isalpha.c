baaada4d0bac9729dc5bf67652815ccba911ea07
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-21 13:48:31 +0200

    made 4 and betty

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - fonction
+ *
+ *@c: vieux truc x2
+ * Description: j'ai juste copié l'exercice d'avant et remis une fonction 
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+	return(1);
+else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
