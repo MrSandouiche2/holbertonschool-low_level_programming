03825a6872a002a98735323ffe15721fafbc91c0
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-24 15:06:29 +0200

    made the 5 and betty

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - fonction
+ *
+ * Description: ça print les nombres mais 10 fois
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j <= 14; j++)
+	{
+		if (j < 10)
+		{
+			_putchar(j + '0');
+
+		}
+else
+{
+	_putchar('1');
+	_putchar(j % 10 + '0');
+}
+	}
+_putchar('\n');
+}
+}
