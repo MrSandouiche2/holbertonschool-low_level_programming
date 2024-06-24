6aedb4c09acf387f7a69ca31eca7e9132eb9ec04
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-24 13:27:54 +0200

    made betty and 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_numbers - fonction
+ *
+ * Description: ça print les nombres
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+
+
+}
+_putchar('\n');
+
+}
