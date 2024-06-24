81c33994062aa1315323977f48b9bb78775c9d0b
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-24 10:21:38 +0200

    made 0 and betty

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - fonction
+ *
+ *@c: le truc
+ * Description: exo qui va return 1 si c'est une maj sinon 0
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+return (0);
+}
