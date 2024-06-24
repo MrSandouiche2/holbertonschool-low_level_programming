b9e6de0e9ee63c8a5ddbf69feaefdc1322aae2ab
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-24 12:00:03 +0200

    made 1 and betty

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isdigit - fonction
+ *
+ *@c: le truc
+ * Description: exo qui va return 1 si c'est un digit sinon ça discobombulate
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int _isdigit(int c)
+
+
+{
+
+if (c >= '0' && c <= '9')
+{
+	return (1);
+}
+else
+return (0);
+}
