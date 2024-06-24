3b0a8bd6fc26aecf5d1c7acfc1007a94f949a7c5
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-24 12:19:53 +0200

    made betty and 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - fonction
+ *
+ *@a: var 1
+ *@b: var 2
+ * Description: ça multiplie a par b
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+
+return (a * b);
+}
