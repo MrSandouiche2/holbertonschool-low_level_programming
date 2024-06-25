301dfb60aca4930428bd049b035b5065682893df
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-25 15:37:26 +0200

    made 8 and betty and fixed something from the 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,6 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 		}
 
 		for (i = 0; i < n; i++)
---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_square - fonction
+ * Description: ça fait un carré
+ *
+ *@size: le tableau de ref
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int i, j;
+if (size <= 0)
+{
+	_putchar('\n');
+}
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+{
+	_putchar('#');
+}
+
+_putchar('\n');
+
+
+}
+
+
+
+
+
+
+}
