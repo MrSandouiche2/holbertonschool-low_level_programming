2f374dc49f17f38f11cbf738a82a1b9e3c8bfe75
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-26 13:59:32 +0200

    changed the name of the folder

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - fonction
+ *
+ * Description: ça retabli le pointeur a 98
+ *@n: norman
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+
+
+}
