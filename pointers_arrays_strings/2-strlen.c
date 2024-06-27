8ef0062c5010190222bdc04eed84513692c3a138
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-27 12:56:10 +0200

    made 2 and betty

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - fonction
+ *
+ * Description: va permettre de print combiens de lettre dans une phrase
+ * @s: pointeur
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while
+(s[i] != '\0') i++;
+
+return (i);
+}
