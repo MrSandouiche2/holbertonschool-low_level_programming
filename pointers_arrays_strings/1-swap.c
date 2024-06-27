57404c37fd06e9ee529ebef4e46dd5253f798e98
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-27 10:09:19 +0200

    made 1 and betty

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * swap_int - fonction
+ *
+ * Description: swap les pointeurs entre eux
+ *@a: pointeur
+ *@b: voir a
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+
+*a = *b;
+
+*b = temp;
+
+
+
+
+
+}
