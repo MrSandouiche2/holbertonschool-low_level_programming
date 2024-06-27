da648cd43862199d2565f9ae1ddd04d7ab9b3180
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-06-27 13:17:23 +0200

    made 3 and betty

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts - fonction
+ *
+ * Description: print un autre pointeur
+ *@str: pointeur
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+{
+	int i;
+
+for (i = 0; str[i] != '\0'; i++)
+
+_putchar(str[i]);
+}
+_putchar('\n');
+
+}
