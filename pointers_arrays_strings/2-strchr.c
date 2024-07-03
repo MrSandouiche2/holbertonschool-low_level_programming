345b6f9f616c004d0c0d08d0ce01f1910ea8b406
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-03 15:54:41 +0200

    made betty and 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strchr - fonction
+ *
+ * Description: va copier la memoire de la zone
+ *@s: pointeur
+ *@c: char
+ * Return: null or the result
+ */
+char *_strchr(char *s, char c)
+{
+
+while (*s != '\0')
+{
+	if (*s == c)
+	{
+		return (s);
+	}
+s++;
+}
+return (NULL);
+}
