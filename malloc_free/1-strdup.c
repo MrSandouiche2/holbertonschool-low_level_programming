5818e2a741063cf5f9ad17bfb1060ed65baa4560
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-09 15:31:03 +0200

    made betty and 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - fonction
+ *
+ * Description: va creer une fonction qui return un pointer dans un espace
+ *@str: pointeur
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+char *_strdup(char *str)
+{
+int i;
+
+char *duplicate;
+
+int length = 0;
+
+	if (str == NULL)
+{
+	return (NULL);
+}
+
+
+while (str[length] != '\0')
+{
+	length++;
+}
+
+
+
+duplicate = (char *)malloc((length + 1) * sizeof(char));
+
+if (duplicate == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i <= length; i++)
+{
+	duplicate[i] = str[i];
+}
+duplicate[length] = '\0';
+return (duplicate);
+}
