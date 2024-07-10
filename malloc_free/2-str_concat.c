9c1451c1f320f844167adb7383c166f2dc5d6786
MrSandouiche2 <8837@holbertonstudents.com>
2024-07-10 15:59:18 +0200

    Update 2-str_concat.c
    lmao i had to edit on github because i can't push from my own pc wtf ?

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 
 char *str_concat(char *s1, char *s2)
 {
 int i, length_s1, length_s2, j;
+length_s1 = 0;
+length_s2 = 0;
+int total_length;
+char *total;
 if (s1 == NULL)
 {
 	length_s1 = 0;
@@ -28,12 +33,21 @@ for (j = 0; s2[j] != '\0'; j++)
 }
 
 }
-int total_length = length_s1 + length_s2 + 1;
+total_length = length_s1 + length_s2 + 1;
 
-char *total = malloc(total_length * sizeof(char));
+*total = malloc(total_length * sizeof(char));
 if (total == NULL)
 {
 	return (NULL);
 }
-
+for (i = 0; i < length_s1; i++)
+{
+	total[i] = s1[i];
+}
+for (j = 0; j < length_s2; j++)
+{
+	total[length_s1 + j] = s2[j];
+}
+total[length_s1 + length_s2] = '\0';
+return (total);
 }
