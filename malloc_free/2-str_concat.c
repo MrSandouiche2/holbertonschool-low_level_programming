38472477f289716d91b0ef9394b53211300914b3
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-10 16:24:21 +0200

    betty is just pissing me off so fuck it

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,53 +1,65 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
-
+/**
+ * str_concat - fonction
+ *
+ * Description: va concatener 2 trucs
+ *@s1: pointeur
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 char *str_concat(char *s1, char *s2)
 {
-int i, length_s1, length_s2, j;
-length_s1 = 0;
-length_s2 = 0;
-int total_length;
+int i, length_s1 = 0, length_s2 = 0, total_length;
 char *total;
-if (s1 == NULL)
-{
-	length_s1 = 0;
-}
-else
-{
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		length_s1++;
-	}
 
+if (s1 != NULL)
+{
+for (i = 0; s1[i] != '\0'; i++)
+{
+length_s1++;
 }
-if (s2 == NULL)
-{
-length_s2 = 0;
-}
-else
-{
-for (j = 0; s2[j] != '\0'; j++)
-{
-	length_s2++;
 }
 
+
+if (s2 != NULL)
+{
+for (i = 0; s2[i] != '\0'; i++)
+{
+length_s2++;
 }
+}
+
+
 total_length = length_s1 + length_s2 + 1;
 
-*total = malloc(total_length * sizeof(char));
+
+
+total = malloc(total_length  *sizeof(char));
 if (total == NULL)
 {
-	return (NULL);
+return (NULL);
 }
+if (s1 != NULL)
+{
 for (i = 0; i < length_s1; i++)
 {
-	total[i] = s1[i];
+total[i] = s1[i];
 }
-for (j = 0; j < length_s2; j++)
+}
+
+
+if (s2 != NULL)
 {
-	total[length_s1 + j] = s2[j];
+for (i = 0; i < length_s2; i++)
+{
+total[length_s1 + i] = s2[i];
 }
+}
+
 total[length_s1 + length_s2] = '\0';
+
 return (total);
 }
