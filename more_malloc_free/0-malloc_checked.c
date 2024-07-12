d3c8a1245f0cf00773c01cdb167f211ab4d525f9
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-12 15:37:51 +0200

    made 0 but doesn't know if it works since the main0 doesn't work

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+void *i;
+
+i = malloc(b);
+if (i == NULL)
+{
+	exit(98);
+}
+return (i);
+
+}
