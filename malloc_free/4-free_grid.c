9378c8dcf511e9a428993511205a2e7856144cae
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-12 14:00:35 +0200

    made betty for 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * free_grid - va free le tableau de alloc_grid
+ * @grid: pointer
+ * @height: the height of the grid.
+ *
+ * Return: pointer to the 2D array, or NULL on failure.
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
