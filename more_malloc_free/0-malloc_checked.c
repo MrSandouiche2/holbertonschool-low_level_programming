00a7b7f15917c121e3641a8b7973576e04a5e442
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-12 15:42:36 +0200

    added the linter to the code

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * malloc_checked - fonction
+ *
+ * Description: ecrit une fonction qui alloue de la memoire
+ *@b: var
+ *
+ *
+ * Return: i
+ */
 void *malloc_checked(unsigned int b)
 {
-void *i;
+	void *i;
 
-i = malloc(b);
-if (i == NULL)
-{
-	exit(98);
-}
-return (i);
-
+	i = malloc(b);
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return (i);
 }
