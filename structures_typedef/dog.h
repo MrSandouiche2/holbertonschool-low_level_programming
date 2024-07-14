c75fb4fc3f7b33ff11268bd9fa9098eb65acfce6
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-14 14:04:41 +0200

    made dog and betty

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defini la structure
+ * @name: premier membre
+ * @age: Second membre
+ * @owner: Troisieme membre
+ *
+ * Description: just a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
