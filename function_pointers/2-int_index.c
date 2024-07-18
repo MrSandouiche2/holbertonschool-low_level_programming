cbdbcf6ecc15c63db1d4251df3bf5bf190af52b6
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-18 10:10:44 +0200

    made 2 and betty

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * int_index - va chercher un integer
+ * @array: pointeur
+ * @size: var
+ *@cmp: pointeur
+ * Return: (-1)
+ */
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+if (array == NULL || size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+
+	if (cmp(array[i]) != 0)
+	{
+		return (i);
+	}
+
+}
+
+return (-1);
+
+}
