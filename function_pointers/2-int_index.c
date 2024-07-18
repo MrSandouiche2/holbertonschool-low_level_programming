3a43ee6e88dd02e895fedbf77facf25887a7e9ec
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-18 10:13:53 +0200

    trying to fix the error

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,33 +1,29 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
- * int_index - va chercher un integer
- * @array: pointeur
- * @size: var
- *@cmp: pointeur
- * Return: (-1)
+ * int_index - Searches for an integer in an array.
+ * @array: Pointer to the array.
+ * @size: Number of elements in the array.
+ * @cmp: Pointer to the function to be used to compare values.
+ * Return: The index of the first matching element,
  */
-
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-if (array == NULL || size <= 0)
-{
-return (-1);
-}
-for (i = 0; i < size; i++)
-{
 
-	if (cmp(array[i]) != 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
-		return (i);
+		return (-1);
 	}
 
-}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
 
-return (-1);
-
+	return (-1);
 }
