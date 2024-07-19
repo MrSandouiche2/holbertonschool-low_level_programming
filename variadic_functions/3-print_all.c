bea9994bfbbac89e776e170ab548be435c180284
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-19 22:41:56 +0200

    maybe the checker was the friend we made along the way

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -37,10 +37,7 @@ void print_all(const char *const format, ...)
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (str)
-					printf("%s", str);
-				else
-					printf("(nil)");
+				printf("%s", str ? str : "(nil)");
 				break;
 			}
 		}
