79c650a19485b2ed6e51c620da084fe082e2a99e
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-01 11:59:51 +0200

    asked my buddy Gepetto for help

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,8 +11,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
+	list_t *new_node, *temp;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
